e979f6ff06acdcce2f232f47581ad60d43f9bcfa
andhy <>
2023-09-19 19:05:18 +0900

    # 課題1 commit
    - 1 src/charge.ts:25
      返却値の型を明示的に
    - 2 src/charge.ts:27
      mapping処理以外でのmapの使用をスタンダードなfor文に
    
    - 3 src/charge.ts:45
      isCoupon処理を関数化

--- assignment-1/src/charge.ts
+++ assignment-1/src/charge.ts
@@ -13,38 +13,36 @@ export type Payment = {
   percentage?: number;
   amount?: number;
 };
+function isCoupon (payments: Payment[]): boolean {
+  for (let i = 0; i < payments.length; i++) {
+    if (payments[i].type !== 'COUPON') {
+      return false;
+    }
+  }
+  return true;
+}
 
-export function charge(invoice: Invoice, payments: Payment[]) {
+export function charge (invoice: Invoice, payments: Payment[]): Receipt {
   const total = invoice.total;
   let deposit = 0;
-
-  payments
-    .sort((payment) => (payment.type !== 'CASH' ? -1 : 1))
-    .map((payment) => {
-      if (payment.type === 'COUPON') {
-        if (payment.percentage != null) {
-          deposit += Math.floor(total * (payment.percentage / 100));
-        } else {
-          deposit += payment.amount || 0;
-        }
+  payments = payments.sort(payment => (payment.type !== 'CASH' ? -1 : 1));
+  for (const payment of payments) {
+    if (payment.type === 'COUPON') {
+      if (payment.percentage != null) {
+        deposit += Math.floor(total * (payment.percentage / 100));
       } else {
-        if (deposit >= total) {
-          throw new Error('OverCharge');
-        }
         deposit += payment.amount || 0;
       }
-    });
+    } else {
+      if (deposit >= total) {
+        throw new Error('OverCharge');
+      }
+      deposit += payment.amount || 0;
+    }
+  }
   if (total > deposit) {
     throw new Error('Shortage');
   }
-
-  let isCoupon = true;
-  for (let i = 0; i < payments.length; i++) {
-    if (payments[i].type !== 'COUPON') {
-      isCoupon = false;
-      continue;
-    }
-  }
-  if (isCoupon) return { total, deposit, change: 0 };
+  if (isCoupon(payments)) return { total, deposit, change: 0 };
   return { total: total, deposit: deposit, change: deposit - total };
 }
